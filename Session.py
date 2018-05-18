4467e32fce036a5ab6759acdcc286ad91604a8b9
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-05-18 14:46:23 +0600

    create session

---
+++ Session.py
@@ -0,0 +1,55 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+app = Flask(__name__)
+app.secret_key = 'any random string'
+
+@app.route('/')
+def index():
+   if 'username' in session:
+      username = session['username']
+      return 'Logged in as ' + username + '<br>' + \
+         "<b><a href = '/logout'>click here to log out</a></b>"
+   return "You are not logged in <br><a href = '/login'></b>" + \
+     "click here to log in</b></a>"
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   if request.method == 'POST':
+      session['username'] = request.form['username']
+      return redirect(url_for('index'))
+   return render_template('session.html')
+
+
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
+
+
+session.html
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+   <form action = "" method = "post">
+       <p>Username <input type ="text" name = "username" /></p>
+       <p><input type = "submit" value = "submit" /></p>
+   </form>
+</body>
+</html>
