21bea9ef10ba6729bacc86a3d0e9e47644f35f36
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-16 11:08:02 +0600

    Create Reading_basic_templates.py

---
+++ Flask_By_Example/Reading_basic_templates.py
@@ -0,0 +1,30 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+ 'cnn': 'http://rss.cnn.com/rss/edition.rss',
+ 'fox': 'http://feeds.foxnews.com/foxnews/latest',
+ 'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route("/")
+@app.route("/bbc")
+def bbc():
+ return get_news('bbc')
+
+@app.route("/cnn")
+def cnn():
+ return get_news('cnn')
+
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+ feed = feedparser.parse(RSS_FEEDS[publication])
+ first_article = feed['entries'][0]
+ return render_template("Home.html")\
+     #.format(first_article.get("title"), first_article.
+#get("published"), first_article.get("summary"))
+
+if __name__ == "__main__":
+ app.run(port=5000, debug=True)
